c8a95e3547b07320cf1ca9b67becc0065c5c4c1d
Gabriela Colombo <65355183+Gabicolombo@users.noreply.github.com>
2024-01-02 22:12:57 -0300

    configurando para receber as primeiras requisições

--- src/app.js
+++ src/app.js
@@ -3,13 +3,18 @@ const body_parser = require('body-parser');
 const cors = require('cors');
 
 const db = require('../db/connection');
+const MovieRoutes = require('./routes/movie');
+const GenreRoutes = require('./routes/genre');
 
 // connecting
 const app = express();
 
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 app.use(body_parser.json());
+
+app.use(MovieRoutes);
+app.use(GenreRoutes);
 
 db();
 

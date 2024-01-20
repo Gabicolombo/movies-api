d865e7ca4131612281ea09cfc5e7104c6c1fdd73
Gabriela Colombo <65355183+Gabicolombo@users.noreply.github.com>
2024-01-20 16:26:42 -0300

    ajuste da url do banco

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 module.exports = async() => {
   try{
 
-    const url = 'mongodb+srv://movies-api:mpxbGD18g6sWzXST@movies-1.43n9fpn.mongodb.net/?retryWrites=true&w=majority';
+    const url = `mongodb+srv://${process.env.user}:${process.env.pwd}@movies-1.43n9fpn.mongodb.net/?retryWrites=true&w=majority`;
     await mongoose.connect(url);
     console.info('Connected to database');
 

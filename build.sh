d502d3f93496ff5617f268a3c11c0bddbf3c82c8
Gabriela Colombo <65355183+Gabicolombo@users.noreply.github.com>
2024-02-02 20:11:31 -0300

    bash build

--- build.sh
+++ build.sh
@@ -3,17 +3,17 @@ echo ""
 echo ""
 echo "Searching for the tag value..."
 path="chart/values.yaml"
-value_tag=$(grep "tag" "$path" | cut -d':' -f2 |tr -d '[:space:]')
-echo "Value: "$value_tag""
+value_tag=$(grep "tag" $path | cut -d':' -f2 |tr -d '[:space:]')
+echo "Value: $value_tag"
 echo ""
 echo ""
 echo ""
 # Creating the image
 echo "Creating the image..."
-docker build -t gcolombo27/movies-api:"$value_tag" -f Dockerfile .
+docker build -t gcolombo27/movies-api:$value_tag -f Dockerfile .
 echo ""
 echo ""
 echo ""
 echo "Pushing the image..."
-docker push gcolombo27/movies-api:"$value_tag"
+docker push gcolombo27/movies-api:$value_tag
 

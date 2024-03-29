5b9daa96299fd192304135ce52fb0f4150aced04
Gabriela Colombo <65355183+Gabicolombo@users.noreply.github.com>
2024-03-29 15:55:23 -0300

    ajuste dos testes para rodar o jenkins e finalizar quando terminar os testes

--- test/movie.test.js
+++ test/movie.test.js
@@ -1,33 +1,60 @@
-
 const supertest = require('supertest');
-const api = require('../src/app');
+const { app, server } = require('../src/app');
+const { disconnectDB } = require('../db/connection');
 
-describe('Testando Endpoints da API de filmes', () => {
+describe('Testando Endpoints da app de filmes', () => {
+
+  afterAll(async() => {
+    await server.close();
+    await disconnectDB();
+  });
+
   it('Deve retornar status 200 ao fazer uma solicitação GET para /movie/2', async () => {
-    const response = await supertest(api).get('/movie/2');
-    expect(response.statusCode).toBe(200);
+    try{
+      const response = await supertest('localhost:2828').get('/movie/2');
+      expect(response.statusCode).toBe(200);
+    }catch(err){
+      console.error(err);
+    }
   });
 
   it('Deve retornar uma mensagem de erro ao fazer uma solicitação GET para /movie/7', async () => {
-    const response = await supertest(api).get('/movie/7');
-    expect(response.text).toEqual("{\"message\":\"No movie found\"}");
+    try{
+      const response = await supertest(app).get('/movie/7');
+      expect(response.text).toEqual("{\"message\":\"No movie found\"}");
+    }catch(err){
+      console.error(err);
+    }
   });
 
   it('Deve retornar status 200 ao fazer uma solicitação GET para /movies', async () => {
-    const response = await supertest(api).get('/movies');
-    expect(response.statusCode).toBe(200);
+    try{
+      const response = await supertest(app).get('/movies');
+      expect(response.statusCode).toBe(200);
+    }catch(err){
+      console.error(err);
+    }
   });
 
   it('Deve retornar uma mensagem de erro ao fazer uma solicitação delete para /movie/7', async () => {
-    const response = await supertest(api).delete('/movie/7');
-    expect(response.statusCode).toBe(404);
-    expect(response.text).toEqual("{\"message\":\"Movie not found\"}");
+    try{
+      const response = await supertest(app).delete('/movie/7');
+      expect(response.statusCode).toBe(404);
+      expect(response.text).toEqual("{\"message\":\"Movie not found\"}");
+    }catch(err){
+      console.log(err);
+    }
   });
 
   it('Deve retornar uma mensagem de erro ao fazer uma solicitação update para /movie/7', async () => {
-    const response = await supertest(api).put('/movie/7');
-    expect(response.statusCode).toBe(404);
-    expect(response.text).toEqual("{\"message\":\"Movie not found\"}");
+    try{
+      const response = await supertest(app).put('/movie/7');
+      expect(response.statusCode).toBe(404);
+      expect(response.text).toEqual("{\"message\":\"Movie not found\"}");
+    }catch(err){
+      console.error(err)
+    }
   });
 
 });
+

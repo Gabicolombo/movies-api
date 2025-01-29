287b4916e74ecae4622e9bc09967d8d6194d73ba
Gabriela Colombo <65355183+Gabicolombo@users.noreply.github.com>
2025-01-29 09:18:53 -0300

    feat: updating the unit test

--- test/movie.test.js
+++ test/movie.test.js
@@ -1,49 +1,146 @@
 const supertest = require('supertest');
-const { app, server } = require('../src/app');
-const { disconnectDB } = require('../db/connection');
+const { app } = require('../src/app');
+const cos = require('ibm-cos-sdk');
+const Movie = require('../src/models/movie');
+const Genre = require('../src/models/genre');
+const mongoose = require('mongoose');
+const { MongoMemoryServer } = require('mongodb-memory-server');
 
-describe('Testando Endpoints da app de filmes', () => {
+let mongoServer;
+
+async function populateMongoLocally() {
+  await Genre.create([{
+    id: 1,
+    name: 'Sci-Fi'
+  }]);
+
+  await Movie.create([
+    {
+      id: 1,
+      title: 'Inception',
+      genre: 'Sci-Fi',
+      image: 'mock-image1',
+      year: 'mock-year1',
+      synopsis: 'mock-synopsis',
+      nameImage: 'mock-image1',
+      runtime: 'runtime-mock1'
+    },
+    {
+      id: 2,
+      title: 'Harry Potter',
+      genre: 'Sci-Fi',
+      image: 'mock-image2',
+      year: 'mock-year2',
+      synopsis: 'mock-synopsis2',
+      nameImage: 'mock-image2',
+      runtime: 'runtime-mock2'
+    },
+  ]);
+}
+
+async function deleteMany() {
+  await Genre.deleteMany();
+  await Movie.deleteMany();
+}
+
+describe('Movie controller', () => {
+  beforeAll(async () => {
+    mongoServer = await MongoMemoryServer.create();
+    const uri = mongoServer.getUri();
+
+    await mongoose.disconnect();
+    await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+  });
 
   afterAll(async () => {
-    await server.close();
-    await disconnectDB();
+    await mongoose.connection.close();
+    await mongoServer.stop();
   });
 
-  it('Deve retornar status 200 ao fazer uma solicitação GET para /movie/2', async () => {
 
-    const response = await supertest('localhost:2828').get('/movie/2');
-    expect(response.statusCode).toBe(200);
+  describe('GET /movies', () => {
 
+    afterAll(async () => {
+      await deleteMany();
+    })
+    it('Should return status 200 and an empty message when no movies are found', async () => {
+      const response = await supertest(app).get('/movies');
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toEqual({ message: 'No movies found' });
+    });
+
+    it('Should return status 200 and a list of movies', async () => {
+      await populateMongoLocally();
+      const response = await supertest(app).get('/movies');
+      expect(response.statusCode).toBe(200);
+      expect(response.body.length).toBe(2);
+      expect(response.body).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({ title: 'Inception' }),
+          expect.objectContaining({ title: 'Harry Potter' }),
+        ])
+      );
+    });
   });
 
-  it('Deve retornar uma mensagem de erro ao fazer uma solicitação GET para /movie/7', async () => {
+  describe('GET /movie', () => {
+    it('Should return status 200 and an empty message when no movies are found', async () => {
+      const response = await supertest(app).get('/movie/1');
+      expect(response.statusCode).toBe(404);
+      expect(response.body).toEqual({ message: 'No movie found' });
+    });
 
-    const response = await supertest(app).get('/movie/7');
-    expect(response.text).toEqual("{\"message\":\"No movie found\"}");
-
+    it('Should return status 200 and a list of movie', async () => {
+      await populateMongoLocally();
+      const response = await supertest(app).get('/movie/1');
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toEqual(
+        expect.objectContaining({ title: 'Inception' }),
+      );
+    });
   });
 
-  it('Deve retornar status 200 ao fazer uma solicitação GET para /movies', async () => {
+  describe('Delete /movie/id', () => {
+    afterAll(async() => {
+      await deleteMany();
+    });
 
-    const response = await supertest(app).get('/movies');
-    expect(response.statusCode).toBe(200);
+    it('should return 200 when find and delete a movie', async() => {
+      
+      const response = await supertest(app).delete('/movie/1');
+      expect(response.statusCode).toBe(200);
+      expect(response.text).toBe("{\"message\":\"Movie deleted successfully\"}");
+    });
+
+    it('should return 404 when the movie not exist', async() => {
+      
+      const response = await supertest(app).delete('/movie/4');
+      expect(response.statusCode).toBe(404);
+      expect(response.text).toBe("{\"message\":\"Movie not found\"}");
+    });
   });
 
-  it('Deve retornar uma mensagem de erro ao fazer uma solicitação delete para /movie/7', async () => {
+  describe('Put /movie/id', () => {
+    beforeAll(async() => {
+      await deleteMany();
+      await populateMongoLocally();
+    });
 
-    const response = await supertest(app).delete('/movie/7');
-    expect(response.statusCode).toBe(404);
-    expect(response.text).toEqual("{\"message\":\"Movie not found\"}");
+    it('should return 200 when find and update the movie', async() => {
+      const response = await supertest(app).put('/movie/1').field('title', 'newTitle');
+      expect(response.statusCode).toBe(200);
+      expect(await Movie.find({ title: 'newTitle'})).toBeDefined();
+      expect(response.text).toBe("{\"message\":\"Movie updated successfully\"}");
+    });
 
-  });
-
-  it('Deve retornar uma mensagem de erro ao fazer uma solicitação update para /movie/7', async () => {
-
-    const response = await supertest(app).put('/movie/7');
-    expect(response.statusCode).toBe(404);
-    expect(response.text).toEqual("{\"message\":\"Movie not found\"}");
-
+    it('should return 404 when the movie not exist', async() => {
+      
+      const response = await supertest(app).put('/movie/4');
+      expect(response.statusCode).toBe(404);
+      expect(response.text).toBe("{\"message\":\"Movie not found\"}");
+    });
   });
 
 });
-
